94bc00182b917d04101e130953f7e4cecd616ac5
alvaro larraya <larraya.132169@e.unavarra.es>
2023-09-18 19:09:30 +0200

    removing astro config netlify import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
