4a86cd5b6715b137611978a826cd52b21c76bc1b
alvaro larraya <larraya.132169@e.unavarra.es>
2023-09-18 19:41:43 +0200

    vercel added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true
   },
   integrations: [react()],
+  output: "hybrid",
+  adapter: vercel()
 });
