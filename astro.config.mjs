a5dbbf10fa71f8df455282a70727d1d81cc3bc2a
alvaro larraya <larraya.132169@e.unavarra.es>
2023-09-18 20:47:34 +0200

    trying to solve back arrow problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from '@astrojs/react';
 
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     drafts: true
