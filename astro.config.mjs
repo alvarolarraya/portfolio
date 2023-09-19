83595168387159918f2b674efa5d0ba3742f42d8
alvaro larraya <larraya.132169@e.unavarra.es>
2023-09-19 18:36:11 +0200

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
+  site:'https://www.alvarolarraya.com/',
   markdown: {
     drafts: true
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: "hybrid",
   adapter: vercel()
 });
